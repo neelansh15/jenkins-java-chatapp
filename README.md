03315bb749b7360321168fb12f9673b0f95b500d
Vedant Mahadik <61352323+VedantMahadik@users.noreply.github.com>
2022-02-26 13:02:27 +0530

    init commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# Java Chat Application
+
+A Java Chat Application
+
+## Usage
+
+1. Pull the code or clone this repo and cd into the working directory
+2. If using VS code just install the redhat java extension pack and run the 2 files simultaneously. Make sure ther server file is run before the client one.
+3. If using the command line open 2 separate instances and cd into the working directory in both of them. One is for Server and other is for Client
+4. cd into the src/chatting/application folder in one of them and run
+
+```bison
+javac Server.java
+      and
+javac Client.java
+```
+
+5. cd back into the working directory
+6. In the command prompt for Server, cd into src and run the command
+
+```bison
+java chatting.application.Server
+```
+
+7. In the command prompt for Client, cd into src and run the command
+
+```bison
+java chatting.application.Client
+```
+
+**Make sure it is done in this order only.**
+
+After setting up the 2 instances you can message between them. Sometimes the message may take a while to appear because of packet loss in that case just message something else from the other client and it should work.  
+&nbsp;
+
+### A working image of the app
+
+&nbsp;
+![image](images/1.png)
+
+## Functionality
+
+It is a light weight java desktop application that allows two users to chat among themselves simultaneously much like whatsapp or any other social media application.
+
+## Features
+
+1. Simple & User-friendly Chat UI design.
+2. Real time chatting on a given I.P Address using Socket Programming(Java Networking).
+3. Additional features such as Chat Bubble, Date & Time of the messages sent & received were also added.
+4. We have also included a scrollbar in our UI framework so that users can scroll back & forth between the messages.
+5. The Status of the user i.e whether he is online ,typing,etc. is also shown while chatting.
+
+## Software Features
+
+1. Ease of use due to a robust GUI.
+2. Simultaneous connectivity over a local server due to the use of web sockets.
+
+## Made by:
+
+1. 1911089 - Vedant Mahadik
+2. 1911093 - Neelansh Mathur
+3. 1911109 - Burhanuddin Rangwala
+4.
+
+![forthebadge made-with-java](http://ForTheBadge.com/images/badges/made-with-java.svg)
---
+++ src/chatting/application/Server.java
@@ -0,0 +1,252 @@
+package chatting.application;
+
+import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.plaf.*;
+import javax.swing.plaf.basic.*;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.net.*;
+import java.io.*;
+
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+
+//Server class that is responsible for user 1
+public class Server implements ActionListener {
+
+    JPanel p1;
+    JTextField t1;
+    JButton b1;
+    static JPanel a1;
+    static JFrame f1 = new JFrame();
+
+    // Creates the box
+    static Box vertical = Box.createVerticalBox();
+
+    static ServerSocket skt;
+    static Socket s;
+    static DataInputStream din;
+    static DataOutputStream dout;
+
+    Boolean typing;
+
+    // Constructor
+    Server() {
+        // The top navbar
+        f1.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        p1 = new JPanel();
+        p1.setLayout(null);
+        p1.setBackground(new Color(7, 94, 84));
+        p1.setBounds(0, 0, 450, 70);
+        f1.add(p1);
+
+        // Setting icon for exit arrow and adding eventlistener to exit
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/3.png"));
+        Image i2 = i1.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel l1 = new JLabel(i3);
+        l1.setBounds(5, 17, 30, 30);
+        p1.add(l1);
+
+        l1.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent ae) {
+                System.exit(0);
+            }
+        });
+
+        // Setting profile picture for user 1
+        ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/pp1.png"));
+        Image i5 = i4.getImage().getScaledInstance(60, 60, Image.SCALE_DEFAULT);
+        ImageIcon i6 = new ImageIcon(i5);
+        JLabel l2 = new JLabel(i6);
+        l2.setBounds(40, 5, 60, 60);
+        p1.add(l2);
+
+        // Setting icon for video
+        ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/video.png"));
+        Image i8 = i7.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+        ImageIcon i9 = new ImageIcon(i8);
+        JLabel l5 = new JLabel(i9);
+        l5.setBounds(290, 20, 30, 30);
+        p1.add(l5);
+
+        // Setting icon for phone
+        ImageIcon i11 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/phone.png"));
+        Image i12 = i11.getImage().getScaledInstance(35, 30, Image.SCALE_DEFAULT);
+        ImageIcon i13 = new ImageIcon(i12);
+        JLabel l6 = new JLabel(i13);
+        l6.setBounds(350, 20, 35, 30);
+        p1.add(l6);
+        // Setting icon for the 3 dots on the side
+        ImageIcon i14 = new ImageIcon(ClassLoader.getSystemResource("chatting/application/icons/3icon.png"));
+        Image i15 = i14.getImage().getScaledInstance(13, 25, Image.SCALE_DEFAULT);
+        ImageIcon i16 = new ImageIcon(i15);
+        JLabel l7 = new JLabel(i16);
+        l7.setBounds(410, 20, 13, 25);
+        p1.add(l7);
+        // Setting the name for user 1
+        JLabel l3 = new JLabel("User 1");
+        l3.setFont(new Font("SAN_SERIF", Font.BOLD, 18));
+        l3.setForeground(Color.WHITE);
+        l3.setBounds(110, 15, 100, 18);
+        p1.add(l3);
+        // Setting the active now label and setting the condition
+        JLabel l4 = new JLabel("Active Now");
+        l4.setFont(new Font("SAN_SERIF", Font.PLAIN, 14));
+        l4.setForeground(Color.WHITE);
+        l4.setBounds(110, 35, 100, 20);
+        p1.add(l4);
+
+        Timer t = new Timer(1, new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                if (!typing) {
+                    l4.setText("Active Now");
+                }
+            }
+        });
+
+        t.setInitialDelay(2000);
+        // Creates the border around the chat section
+        a1 = new JPanel();
+        a1.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
+
+        JScrollPane sp = new JScrollPane(a1);
+        sp.setBounds(5, 75, 440, 570);
+        sp.setBorder(BorderFactory.createEmptyBorder());
+        // Scrollbar logic and UI
+        ScrollBarUI ui = new BasicScrollBarUI() {
+            protected JButton createDecreaseButton(int orientation) {
+                JButton button = super.createDecreaseButton(orientation);
+                button.setBackground(new Color(7, 94, 84));
+                button.setForeground(Color.WHITE);
+                this.thumbColor = new Color(7, 94, 84);
+                return button;
+            }
+
+            protected JButton createIncreaseButton(int orientation) {
+                JButton button = super.createIncreaseButton(orientation);
+                button.setBackground(new Color(7, 94, 84));
+                button.setForeground(Color.WHITE);
+                return button;
+            }
+        };
+
+        sp.getVerticalScrollBar().setUI(ui);
+        f1.add(sp);
+        // Additional logic for setting typing or active now
+        t1 = new JTextField();
+        t1.setBounds(5, 655, 310, 40);
+        t1.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
+        f1.add(t1);
+
+        t1.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent ke) {
+                l4.setText("typing...");
+
+                t.stop();
+
+                typing = true;
+            }
+
+            public void keyReleased(KeyEvent ke) {
+                typing = false;
+
+                if (!t.isRunning()) {
+                    t.start();
+                }
+            }
+        });
+
+        // Setting the send button
+        b1 = new JButton("Send");
+        b1.setBounds(320, 655, 123, 40);
+        b1.setBackground(new Color(7, 94, 84));
+        b1.setForeground(Color.WHITE);
+        b1.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
+        b1.addActionListener(this);
+        f1.add(b1);
+
+        f1.getContentPane().setBackground(Color.WHITE);
+        f1.setLayout(null);
+        f1.setSize(450, 700);
+        f1.setLocation(400, 200);
+        f1.setUndecorated(true);
+        f1.setVisible(true);
+
+    }
+
+    // logic for sending the message
+    public void actionPerformed(ActionEvent ae) {
+        try {
+            String out = t1.getText();
+
+            JPanel p2 = formatLabel(out);
+
+            a1.setLayout(new BorderLayout());
+
+            JPanel right = new JPanel(new BorderLayout());
+            right.add(p2, BorderLayout.LINE_END);
+            vertical.add(right);
+            vertical.add(Box.createVerticalStrut(15));
+
+            a1.add(vertical, BorderLayout.PAGE_START);
+
+            dout.writeUTF(out);
+            t1.setText("");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    // Speech bubble
+    public static JPanel formatLabel(String out) {
+        JPanel p3 = new JPanel();
+        p3.setLayout(new BoxLayout(p3, BoxLayout.Y_AXIS));
+
+        JLabel l1 = new JLabel("<html><p style = \"width : 150px\">" + out + "</p></html>");
+        l1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        l1.setBackground(new Color(153, 255, 153));
+        l1.setOpaque(true);
+        l1.setBorder(new EmptyBorder(15, 15, 15, 50));
+
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+
+        JLabel l2 = new JLabel();
+        l2.setText(sdf.format(cal.getTime()));
+
+        p3.add(l1);
+        p3.add(l2);
+        return p3;
+    }
+
+    // main function for setting server and ip stuff
+    public static void main(String[] args) {
+        new Server().f1.setVisible(true);
+
+        String msginput = "";
+        try {
+            skt = new ServerSocket(6001);
+            while (true) {
+                s = skt.accept();
+                din = new DataInputStream(s.getInputStream());
+                dout = new DataOutputStream(s.getOutputStream());
+
+                while (true) {
+                    msginput = din.readUTF();
+                    JPanel p2 = formatLabel(msginput);
+
+                    JPanel left = new JPanel(new BorderLayout());
+                    left.add(p2, BorderLayout.LINE_START);
+                    vertical.add(left);
+                    f1.validate();
+                }
+
+            }
+
+        } catch (Exception e) {
+        }
+    }
+}
